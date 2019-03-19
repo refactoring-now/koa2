4e1ba514d3b2d017f54db6fdec918bcbf3a7fe3e
yangkun <18070924@cnsuning.com>
2019-03-19 20:57:00 +0800

    feat: 初步完善

--- benchmarks/app.js
+++ benchmarks/app.js
@@ -2,4 +2,3 @@ const Koa2 = require('../lib/application')
 
 const app = new Koa2()
 app.listen(2333)
-
--- examples/middleware.js
+++
@@ -1,7 +0,0 @@
-
-const Koa2 = require('../lib/application')
-const app = new Koa2()
-
-async function timeout() {
-
-}
